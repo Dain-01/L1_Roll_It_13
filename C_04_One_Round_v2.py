9143682b3c2dcf7a172e3cec7f9e2c309e84cd54
Dain-01 <ioaneo60807@masseyhigh.school.nz>
2026-03-26 13:37:29 +1300

    UNFINISHED CODE (Video 10) - Single round v2

---
+++ C_04_One_Round_v2.py
@@ -0,0 +1,52 @@
+import random
+
+def initial_points(which_player):
+    """Roll dice twice and return total / if double points apply"""
+
+    double = "no"
+
+    # Roll the dice for the user and note if they got a double
+
+
+
+    roll_one = random.randint(1, 6)
+    roll_two = random.randint(1, 6)
+
+    if roll_one == roll_two:
+        double = "yes"
+
+
+    total = roll_one + roll_two
+    print(f"{which_player}   - Roll 1: {roll_one} \t| Roll 2: {roll_two} \t| Total: {total} ")
+
+    return total, double
+
+# Main starts here. . .
+
+
+
+
+
+# Follows the "retrieve user points" command / function
+initial_user = initial_points("User")
+initial_comp = initial_points("Comp")
+
+
+
+# Retrieve user points (first item returned from function)
+user_points = initial_user[0]
+comp_points = initial_comp[0]
+
+double_user = initial_user[1]
+
+
+
+#Let the user know if they qualify for double points
+if double_user == "yes":
+    print("Great news - if you win, you will earn double points!!")
+
+# assume user goes first. . .
+first = "User"
+second = "Computer"
+player_1_points = user_points
+player_2_points = comp_points
